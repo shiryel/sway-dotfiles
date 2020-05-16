4a7874947e2db7226bcc69c75fb06ccb524a650a
shiryel <shiryel@hotmail.com>
2020-05-15 21:20:15 -0300

    fix zsh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,7 +6,14 @@ for i in `ls "$SCRIPTPATH/dotfiles"`; {
   ln -fsv "$SCRIPTPATH/dotfiles/$i" "$HOME/.$i"
 }
 
+mkdir -p "$HOME/.config"
+
 for i in `ls "$SCRIPTPATH/config"`; {
+  # FIXME: when is first time will show a error
   rm -vr "$HOME/.config/$i"
   ln -fsv "$SCRIPTPATH/config/$i" "$HOME/.config/$i"
 }
+
+# Instal OhMyZSH and theme
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+wget -O /home/shiryel/.oh-my-zsh/themes/bullet-train.zsh-theme https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
